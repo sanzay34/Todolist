5fb30e7b2641e54f1681784a63d96acc31869cb9
sanzay34 <sanzay321@gmail.com>
2025-02-17 21:26:51 +0545

    Fix TodoItems import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import TodoContainer from "./components/TodoContainer";
 
-// const App = () => {
-//   return (
-//     <div className="w-screen h-screen">
-//       <ParentComponent />
-//     </div>
-//   );
-// };
+
 
 const App = () => {
 	return (
