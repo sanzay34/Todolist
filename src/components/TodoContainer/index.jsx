d6c536b8eba6b07166d432764f854eecefd1034e
sanzay34 <sanzay321@gmail.com>
2025-02-18 11:16:20 +0545

    Final ommit

--- src/components/TodoContainer/index.jsx
+++ src/components/TodoContainer/index.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import TodoForm from "./Todoform";
 import TodoItems from "./Todoitems";
-import TodoFilters from "./Todofilters";
+import TodoFilters from "./TodoFilters";
 import { ACTION_TYPES } from "../../context/TodoContext";
 import { setTodoItems } from "../../features/todos/todoSlice";
 import { useDispatch, useSelector } from "react-redux";
